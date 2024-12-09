93cc58e6aa0fca01fdfba5d641e6003820da95b6
kieransweetman <kikisweetman@gmail.com>
2024-12-09 15:50:00 +0100

    small optimization for 'better' ux + formatting

--- islands/TypewriterIntro.tsx
+++ islands/TypewriterIntro.tsx
@@ -2,79 +2,85 @@ import { useEffect, useState } from "preact/hooks";
 import Typewriter from "./Typewriter.tsx";
 
 const messages = [
-    "if you could send a message...",
-    "anonymous, untraceable...",
-    "or with your name...",
-    "what would you say?",
+  "if you could send a message...",
+  "anonymous, untraceable...",
+  "or with your name...",
+  "what would you say?",
 ];
 
 const speeds = [50, 100, 75, 50]; // Define speeds for each message
 
 export default function TypewriterIntro() {
-    const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
-    const [skip, setSkip] = useState(false);
-    const [fadeOut, setFadeOut] = useState(false);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [skip, setSkip] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
 
-    const handleComplete = () => {
-        if (currentMessageIndex < messages.length - 1) {
-            setCurrentMessageIndex(currentMessageIndex + 1);
-        }
+  const handleComplete = () => {
+    if (currentMessageIndex < messages.length - 1) {
+      setCurrentMessageIndex(currentMessageIndex + 1);
+    }
+
+    if (currentMessageIndex == messages.length - 1) {
+      setTimeout(() => {
+        globalThis.location.href = "/space";
+      }, 1500);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+        setFadeOut(true);
+        setTimeout(() => {
+          setSkip(true);
+          setFadeOut(false);
+        }, 2000); // Wait for fade-out animation to complete
+      }
     };
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.code === "Space") {
-                setFadeOut(true);
-                setTimeout(() => {
-                    setSkip(true);
-                    setFadeOut(false);
-                }, 2000); // Wait for fade-out animation to complete
-            }
-        };
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+        setSkip(false);
+      }
+    };
 
-        const handleKeyUp = (event: KeyboardEvent) => {
-            if (event.code === "Space") {
-                setSkip(false);
-            }
-        };
+    globalThis.addEventListener("keydown", handleKeyDown);
+    globalThis.addEventListener("keyup", handleKeyUp);
 
-        globalThis.addEventListener("keydown", handleKeyDown);
-        globalThis.addEventListener("keyup", handleKeyUp);
+    return () => {
+      globalThis.removeEventListener("keydown", handleKeyDown);
+      globalThis.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
-        return () => {
-            globalThis.removeEventListener("keydown", handleKeyDown);
-            globalThis.removeEventListener("keyup", handleKeyUp);
-        };
-    }, []);
+  useEffect(() => {
+    if (skip) {
+      setCurrentMessageIndex(messages.length); // Set index beyond the last message
+      setTimeout(() => {
+        globalThis.location.href = "/space";
+      }, 1250);
+    }
+  }, [skip]);
 
-    useEffect(() => {
-        if (skip) {
-            setCurrentMessageIndex(messages.length); // Set index beyond the last message
-            setTimeout(() => {
-                globalThis.location.href = "/space";
-            }, 1500);
-        }
-    }, [skip]);
+  return (
+    <div class="flex justify-center items-center bg-gray-900 h-[100vh] w-[100vw]">
+      {!skip && currentMessageIndex < messages.length && (
+        <Typewriter
+          key={currentMessageIndex}
+          className={`text-white ${fadeOut ? "fade-out" : ""}`}
+          speed={speeds[currentMessageIndex]} // Use the speed corresponding to the current message
+          onTextRendered={handleComplete}
+        >
+          {messages[currentMessageIndex]}
+        </Typewriter>
+      )}
 
-    return (
-        <div class="flex justify-center items-center bg-gray-900 h-[100vh] w-[100vw]">
-            {!skip && currentMessageIndex < messages.length && (
-                <Typewriter
-                    key={currentMessageIndex}
-                    className={`text-white ${fadeOut ? "fade-out" : ""}`}
-                    speed={speeds[currentMessageIndex]} // Use the speed corresponding to the current message
-                    onTextRendered={handleComplete}
-                >
-                    {messages[currentMessageIndex]}
-                </Typewriter>
-            )}
-
-            <div
-                className={(skip ? "fade-out" : "") +
-                    " absolute bottom-4 text-white"}
-            >
-                Press space to skip
-            </div>
-        </div>
-    );
+      <div
+        className={(skip ? "fade-out" : "") +
+          " absolute bottom-4 text-white"}
+      >
+        Press space to skip
+      </div>
+    </div>
+  );
 }
--- routes/space/index.tsx
+++ routes/space/index.tsx
@@ -9,13 +9,15 @@ export default async function SpaceIndex(_req: Request, ctx: FreshContext) {
   return (
     <div class="relative flex flex-col gap-5 justify-center items-center  h-[100vh] w-[50vw]">
       <div className="flex flex-col overflow-y-auto h-[80%] z-[100]">
-        {messages?.map(({ to, from, message }) => (
-          <div class=" p-4 m-4 rounded-lg">
-            <h2 class="text-xl font-semibold">{to}</h2>
-            {from && <h2 class="text-xl font-semibold">{from}</h2>}
-            <p>{message}</p>
-          </div>
-        ))}
+        {messages
+          ? messages?.map(({ to, from, message }) => (
+            <div class=" p-4 m-4 rounded-lg">
+              <h2 class="text-xl font-semibold">{to}</h2>
+              {from && <h2 class="text-xl font-semibold">{from}</h2>}
+              <p>{message}</p>
+            </div>
+          ))
+          : null}
       </div>
       <CanvasIsland />
       <MessageForm />
