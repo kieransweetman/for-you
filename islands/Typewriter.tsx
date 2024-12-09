0535eb29de0ac3c790a5552820dc3dd746b4030a
kieransweetman <kikisweetman@gmail.com>
2024-12-09 16:04:11 +0100

    linting

--- islands/Typewriter.tsx
+++ islands/Typewriter.tsx
@@ -2,33 +2,33 @@ import { useTypewriter } from "../hooks/use-typewriter.ts";
 import { useEffect, useState } from "preact/hooks";
 
 interface TypewriterProps {
-    speed?: number;
-    className?: string;
-    children: string;
-    onTextRendered?: () => void;
+  speed?: number;
+  className?: string;
+  children: string;
+  onTextRendered?: () => void;
 }
 
 const Typewriter = (
-    { speed, className, children, onTextRendered }: TypewriterProps,
+  { speed, className, children, onTextRendered }: TypewriterProps,
 ) => {
-    const displayText = useTypewriter(children, speed);
-    const [isComplete, setIsComplete] = useState(false);
+  const displayText = useTypewriter(children, speed);
+  const [isComplete, setIsComplete] = useState(false);
 
-    useEffect(() => {
-        if (displayText === children) {
-            setIsComplete(true);
+  useEffect(() => {
+    if (displayText === children) {
+      setIsComplete(true);
 
-            if (onTextRendered) {
-                setTimeout(onTextRendered, 1500);
-            }
-        }
-    }, [displayText, children, onTextRendered]);
+      if (onTextRendered) {
+        setTimeout(onTextRendered, 1500);
+      }
+    }
+  }, [displayText, children, onTextRendered]);
 
-    return (
-        <p className={`${className} ${isComplete ? "fade-out" : ""}`}>
-            {displayText}
-        </p>
-    );
+  return (
+    <p className={`${className} ${isComplete ? "fade-out" : ""}`}>
+      {displayText}
+    </p>
+  );
 };
 
 export default Typewriter;
